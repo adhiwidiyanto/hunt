29001f31e836c925b727402b71e4d56e51a28050
mrkentung <widiyanto.adhi@gmail.com>
2018-07-05 13:52:30 +0700

    Login , Register module

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,13 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
+const passport = require('passport')
+
+// Load input validation
+const validateRegisterInput = require('../../validation/registration')
+const validateLoginInput = require('../../validation/login')
 
 // Load User Model
 const User = require('../../models/Users')
@@ -15,11 +22,19 @@ router.get('/test', (req, res) => res.json({msg: "Users work"}))
 // @desc    Test users route
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body)
+
+  // Check Validation
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
+
   User.findOne({
     email: req.body.email
   }).then(user => {
     if(user) {
-      return res.status(400).json({email: 'Email already exists'})
+      errors.email = 'Email already exists'
+      return res.status(400).json(errors)
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size
@@ -45,6 +60,51 @@ router.post('/register', (req, res) => {
       })
     }
   })
+})
+
+// @route   GET api/users/login
+// @desc    Login users route / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+
+  // Check Validation
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
+
+  const email = req.body.email
+  const password = req.body.password
+
+  // Find user by email
+  User.findOne({email})
+    .then(user => {
+      // Check for user
+      if(!user) {
+        errors.email = 'User not found'
+        return res.status(404).json(errors)
+      }
+
+      // Check Password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            //User Matched
+            const payload = {id: user.id, name: user.name, avatar: user.avatar } // Create JWT Payload
+            
+            //Sign Token
+            jwt.sign(payload, keys.secretKey, {expiresIn: 3600}, (err, token) => {
+              res.json({
+                success: true,
+                token: `Bearer ${token}`
+              })
+            })
+          } else {
+            errors.password = 'Password incorrect'
+            return res.status(400).json(errors)
+          }
+        })
+    })
 })
 
 module.exports = router
